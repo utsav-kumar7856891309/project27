45a504c6aca4c4fc327f6704381b37d42d909c56
utsav-kumar7856891309 <15102004utsavkumar@gmail.com>
2025-04-06 22:27:13 +0530

    this is todo-list-app

--- index.js
+++ index.js
@@ -1,67 +1,32 @@
-class GCObject {
-    static idCounter = 1;
-
-    constructor(generation = 0) {
-        this.id = GCObject.idCounter++;
-        this.generation = generation;
-        this.refCount = 1;
-        console.log(Object ${this.id} created in generation ${this.generation});
+const inputbox=document.getElementById("input-box");
+const listcontainer=document.getElementById("list-container");
+function addtask(){
+    if(inputbox.value===''){
+        alert("You must write somthing");
+    }else{
+        let li=document.createElement("li");
+        li.innerHTML=inputbox.value;
+        listcontainer.appendChild(li);
+        let span=document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
     }
-
-    addReference() {
-        this.refCount++;
-    }
-
-    removeReference() {
-        this.refCount--;
-        if (this.refCount === 0) {
-            console.log(Object ${this.id} collected from generation ${this.generation});
-        }
-    }
+    inputbox.value="";
+    savedata();
 }
-
-class GarbageCollector {
-    constructor() {
-        this.generations = [[], [], []];
-        this.objects = new Map();
+listcontainer.addEventListener("click",function(e){
+    if(e.target.tagName==="LI"){
+        e.target.classList.toggle("changed");
+        savedata();
+    }else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove();
+        savedata();
     }
-
-    allocate(gen = 0) {
-        const obj = new GCObject(gen);
-        this.generations[gen].push(obj);
-        this.objects.set(obj.id, obj);
-        return obj;
-    }
-
-    collect(gen) {
-        console.log(\nRunning Garbage Collection for Generation ${gen});
-        this.generations[gen] = this.generations[gen].filter(obj => {
-            if (obj.refCount === 0) {
-                this.objects.delete(obj.id);
-                return false;
-            }
-            return true;
-        });
-    }
-
-    promote(obj) {
-        if (obj.generation < 2) {
-            console.log(Promoting Object ${obj.id} to Generation ${obj.generation + 1});
-            this.generations[obj.generation] = this.generations[obj.generation].filter(o => o !== obj);
-            obj.generation++;
-            this.generations[obj.generation].push(obj);
-        }
-    }
+},false);
+function savedata(){
+    localStorage.setItem("data",listcontainer.innerHTML);
 }
-
-// Example usage
-const gc = new GarbageCollector();
-const obj1 = gc.allocate();
-const obj2 = gc.allocate();
-const obj3 = gc.allocate(1);
-
-obj1.removeReference();
-gc.collect(0);
-
-gc.promote(obj2);
-gc.collect(1);
+function showtask(){
+    listcontainer.innerHTML=localStorage.getItem("data");
+}
+showtask();
